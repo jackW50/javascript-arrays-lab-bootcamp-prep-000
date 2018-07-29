6ca71eb4914ad80ebef4e375019158a340ba303a
John Werenski <jweredc@gmail.com>
2018-07-29 18:56:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function destructivelyPrependKitten(name) {
   var newList2 = kittens.unshift(name);
   return newList2
 }
+
+function destructivelyRemoveLastKitten() {
+  var newList3 = kittens.pop()
+  return newList3
+}
+
+function destructivelyRemoveFisrtKitten() {
+  var newList4 = kittens.shift()
+  return newList4
+}
